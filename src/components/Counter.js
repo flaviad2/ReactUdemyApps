df8dd5470eb80dd331913c09ceae75c64bb63c4c
flaviad2 <100410860+flaviad2@users.noreply.github.com>
2023-01-23 12:18:16 +0200

    added redux counter app

---
+++ src/components/Counter.js
@@ -0,0 +1,43 @@
+import classes from './Counter.module.css';
+
+import {counterActions} from '../store/counter'
+
+import { useSelector, useDispatch } from 'react-redux'
+
+const Counter = () => {
+  const dispatch = useDispatch()
+  const counter = useSelector((state) => state.counter.counter)
+  const show = useSelector((state) => state.counter.showCounter)
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment())
+  }
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(10)) // {type: SOME_UNIQUE_IDENTIFIER, payload: 10}
+  }
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement())
+  }
+
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={increaseHandler}>Increase by 5 </button>
+        <button onClick={decrementHandler}>decrement</button>  
+      </div> 
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+// import {createStore} from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+import counterReducer from './counter';
+import authReducer from './auth'
+
+
+
+
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+        auth: authReducer
+    }
+})
+
+
+
+
+export default store
